9d876e0ad518ce6bc276f3eac2745236ee7dfedf
Allan <allan.averlan@laposte.net>
2022-11-07 14:21:11 +0100

    recherche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,6 +10,7 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
@@ -50,11 +51,10 @@ class HomeController extends AbstractController
      * @Route("/showArticles/{id}", name="show_articles")
      */
     public function showArticles(
-        Category $category,
-        PaginatorInterface $paginator,
-        Request $request
+        ?Category $category,
+        Request $request,
+        PaginatorInterface $paginator
     ): Response {
-        
         $categories = $this->repoCategory->findAll();
         $articles = array_reverse($this->repoArticle->findAll());
 
@@ -71,7 +71,7 @@ class HomeController extends AbstractController
         }
         return $this->render('home/index.html.twig', [
             'articles' => $articlesPag,
-            'categories' => $categories,
+            'categories' => $this->repoCategory->findAll(),
         ]);
     }
 
@@ -85,6 +85,46 @@ class HomeController extends AbstractController
         }
         return $this->render('home/show.html.twig', [
             'article' => $article,
+        ]);
+    }
+    /**
+     * @Route("/recherche", name="recherche")
+     */
+    public function recherche(
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response {
+        $date = \DateTime::createFromFormat(
+            'Y-m-d',
+            date($request->request->get('date'))
+        );
+        //dd($date);
+
+        $session = new Session();
+        if (!$session->get('articles')) {
+            $session->start();
+        }
+
+        if ($request->request->get('title')) {
+            $articles = $this->repoArticle->findByTitleLike(
+                $request->request->get('title'),
+                $date
+            );
+            $session->set('articles', $articles);
+        }
+
+        $articlesPag = $paginator->paginate(
+            $session->get('articles'), // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+
+        $recherche = (string) $request->request->get('title');
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articlesPag,
+            'categories' => $this->repoCategory->findAll(),
+            'titre' => "Articles de la recherche :  $recherche",
         ]);
     }
 }
